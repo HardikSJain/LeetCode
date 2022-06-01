a0a42f4630877fb608f94cfa0f8d1506f0d1d61a
Hardik <hardik.sj@somaiya.edu>
2022-06-02 01:36:00 +0530

    add 412

---
+++ cpp_solutions/_412.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<string> fizzBuzz(int n) {
+    vector<string> ans;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i%15==0)
+        {
+            ans.push_back("FizzBuzz");
+        }
+        else if(i%3==0){
+            ans.push_back("Fizz");
+        }
+        else if(i%5==0){
+            ans.push_back("Buzz");
+        }
+        else
+        {
+            ans.push_back(to_string(i));
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    int n = 3;
+    vector<string> ans = fizzBuzz(n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<ans[i];
+    }
+    
+}
