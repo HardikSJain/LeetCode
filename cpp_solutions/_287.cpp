07385907d24cb6c400dded8ab3f00454b21ef5c8
Hardik <hardik.sj@somaiya.edu>
2022-06-27 22:32:01 +0530

    added: 287

--- cpp_solutions/_287.cpp
+++ cpp_solutions/_287.cpp
@@ -1,25 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-
-// time limit exceeded
-int main(int argc, char const *argv[])
+int findDuplicate(vector<int> &nums)
 {
-    vector<int> nums = {1,3,4,2,2};
-    int ans;
+    // FLOYD CYCLE DETECTION
+    int slow = nums[0];
+    int fast = nums[nums[0]];
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }
+    slow = 0;
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+    return slow;
+}
+
+int findDuplicate1(vector<int> &nums)
+{
+    // COUNT VECTOR
+    vector<int> cnt(nums.size(), 0);
+    int res = 0;
+
     for (int i = 0; i < nums.size(); i++)
     {
-        ans = nums[i];
-        for (int j = i + 1; j < nums.size(); j++)
+        cnt[nums[i]]++;
+    }
+
+    for (int i = 0; i < cnt.size(); i++)
+    {
+        if (cnt[i] > 1)
         {
-            if (ans == nums[j])
-            {
-                cout<<nums[j];
-                break;
-            }
+            res = i;
+            break;
         }
     }
-    
+
+    return res;
+}
+
+// slow = 1; fast = 3
+
+// slow = 3; fast = 4
+// slow = 2; fast = 4
+// slow = 4; fast = 4
+
+// slow = 0;
+// slow = 1; fast = 4
+// slow = 3; fast = 4
+// slow = 2; fast = 4
+// slow = 4; fast = 4
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {1, 3, 4, 2, 2};
+    cout << findDuplicate(nums) << endl;
     return 0;
 }
