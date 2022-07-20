462dac116e60448c1c750aad67b4dd0fb2e4e241
Hardik <hardik.sj@somaiya.edu>
2022-07-20 17:58:26 +0530

    updated: 242

--- cpp_solutions/_242.cpp
+++ cpp_solutions/_242.cpp
@@ -2,93 +2,60 @@
 
 using namespace std;
 
-// RIGHT brute force approach (first attempt)
-void bruteforce()
+bool isAnagram(string s, string t)
 {
-    string s;
-    string t;
-    cin >> s >> t;
     int n = s.size();
     int m = t.size();
-    int flag = 1;
-    for (int i = 0; i < m; i++)
-    {
-        int j = 0;
-        while (t[i] != s[j])
-        {
-            j++;
-            if (j >= n)
-            {
-                break;
-            }
-        }
-        if (t[i] == s[j])
-        {
-            s[j] = -1;
-            flag *= 1;
-        }
-        else
-        {
-            flag *= 0;
-            cout << "false" << endl;
-            break;
-        }
-    }
-    if (flag == 1)
-    {
-        cout << "true" << endl;
-    }
-}
-
-// WRONG counting ASCII (second attempt)
-// wrong because there may be cases where addition of ascii of all the characters can be equal to sum of some other chars
-// for example a+c and b+b
-void countingASCII()
-{
-    string s;
-    string t;
-    cin >> s >> t;
-    int n = s.size();
-    int m = t.size();
-    int count = 0;
     if (n != m)
     {
-        cout << "false" << endl;
-        return;
+        return false;
     }
+
+    unordered_map<char, int> mp;
 
     for (int i = 0; i < n; i++)
     {
-        count += int(s[i]);
-        count -= int(t[i]);
+        mp[s[i]]++;
     }
-    if (count == 0)
+
+    for (int i = 0; i < m; i++)
     {
-        cout << "true" << endl;
+        if (mp.find(t[i]) == mp.end())
+        {
+            return false;
+        }
+        else
+        {
+            mp[t[i]]--;
+            if (mp[t[i]] == 0)
+            {
+                mp.erase(t[i]);
+            }
+        }
     }
-    else
-    {
-        cout << "false" << endl;
-    }
+    return mp.size() == 0;
 }
+// bool isAnagram(string s, string t)
+// {
+//     int n = s.size();
+//     int m = t.size();
+//     sort(s.begin(), s.end());
+//     sort(t.begin(), t.end());
+//     if (s == t)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
 
-// Sorting and comparing (saw solution)
-
-void sorting()
+int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     string s;
     string t;
     cin >> s >> t;
-    int n = s.size();
-    int m = t.size();
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
-    if (s == t)
-    {
-        cout << "true" << endl;
-    }
-    else
-    {
-        cout << "false" << endl;
-    }
+    bool res = isAnagram(s, t);
+    return 0;
 }
