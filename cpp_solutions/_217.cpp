7db03f0b2f3d0f5ec6e4018bfb0bd3b3db9f20ab
Hardik <hardik.sj@somaiya.edu>
2022-08-29 16:16:48 +0530

    updated: 217

--- cpp_solutions/_217.cpp
+++ cpp_solutions/_217.cpp
@@ -1,8 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// RIGHT first attempt
-void soln()
+bool containsDuplicate(vector<int> &nums)
+{
+    unordered_set<int> s;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (s.find(nums[i]) != s.end())
+        {
+            return true;
+        }
+        s.insert(nums[i]);
+    }
+    return false;
+}
+
+void containsDuplicate1()
 {
     vector<int> nums;
     int n = nums.size();
@@ -16,4 +29,32 @@ void soln()
         }
     }
     cout << "false" << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        if (containsDuplicate(a))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
 }
